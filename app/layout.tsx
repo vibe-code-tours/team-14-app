5468b06178557f41dfcc33f1d9699d40508d2707
Zin Mar <zinmar.ucsy1@gmail.com>
2026-07-12 13:17:03 +0630

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-100 font-sans antialiased transition-colors">
+      <body className="min-h-screen bg-slate-50 text-slate-900 dark:text-slate-100 font-sans antialiased transition-colors">
 
         <SessionProvider>
           <LanguageProvider>{children}</LanguageProvider>
